d49c380ab8f39e17ad0a9c07200b79fa0f99ddcb
James Bedford <jamesbedford01@hotmail.com>
2022-02-12 08:51:06 +0000

    Start of day 12-2-2022 v.06

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //Get screen dimensions to allow squares to move as much as possible.
+  let screenWidth = window.innerWidth;
+  let screenHeight = window.innerHeight;
+  //screenWidth slightless than screen so square remains visible
+  screenWidth = screenWidth - 60;
+  screenHeight = screenHeight - 60;
+  let foo = window.innerWidth;
+
+  console.log(`adjusted screenW ${screenWidth} vs ${foo}`);
+  //console.log(window.innerHeight);
   let button = document.createElement(`button`);
   let buttonTxt = document.createTextNode(`Add Square`);
   button.appendChild(buttonTxt);
@@ -10,8 +20,10 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   function square(j) {
+    //Create parent div
     let squareContainer = document.createElement(`div`);
     squareContainer.classList.add(`squareContainer`);
+    //Create square div
     let square = document.createElement(`div`);
     square.classList.add(`square`);
     squareContainer.appendChild(square);
@@ -21,23 +33,10 @@ document.addEventListener("DOMContentLoaded", function () {
     square.addEventListener(`click`, function () {
       square.style.backgroundColor = rainbow();
     });
-    //disp();
-    let i = 0;
-    //stimer();
 
-    while (i < 80) {
-      //setInterval(disp(), 2000);
-
-      let x = document.getElementById("sq-1").offsetLeft;
-      if (x < 800) {
-        x = x + 1;
-        //setTimeout(console.log(`timer fired`), 10);
-        // stimer();
-        document.getElementById("sq-1").style.left = x + "px"; //horizontal )
-        console.log(`After ${x}`);
-      }
-      i++;
-    } // end While
+    let squ = document.getElementById("sq-1");
+    //squareEngine(squ, `right`, (distance = 200), (duration = 5000));
+    squareEngine(squ, `right`, screenWidth, (duration = 5000));
   }
   //Random RGB color generator function
   function rainbow() {
@@ -52,42 +51,76 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   //Function to give movment to the squares
-  function squareEngine(square) {
-    let left = 0;
 
-    for (let i = 0; i < 900; i++) {
-      left++;
-      console.log(left);
-      return (square.style.left = left + "px");
+  function xmoveRight(element) {
+    let elStyle = window.getComputedStyle(element);
+    let leftValue = elStyle.getPropertyValue("left").replace("px", "");
+    element.style.left = Number(leftValue) + 600 + "px";
+  }
+
+  function squareEngine(element, direction, distance = 20, duration = 1000) {
+    // Chose left or right and it picks right
+    let topOrLeft =
+      direction === "left" || direction === "right" ? "left" : "top";
+    let isNegated = direction === "up" || direction === "left";
+    console.log(isNegated);
+    //Sub condition then makes teh chocie of left correct  as it was right
+    //if movement is left or up minus the number to get the opposite direction
+    if (isNegated) {
+      distance *= -1;
+      console.log(distance);
     }
-  }
+    // get the full .css value of the element
+    //Its overkill but want to get use to using it.
+    let squareStyle = window.getComputedStyle(element);
+    let value = squareStyle.getPropertyValue(topOrLeft).replace("px", "");
+    let destination = Number(value) + distance;
+    let frameDistance = distance / (duration / 10);
 
-  function stimer() {
-    disp();
-    my_time = setTimeout(stimer(), 1000);
-  }
-
-  function disp() {
-    let step = 1; // Change this step value
-    let y = document.getElementById("sq-1").offsetTop;
-    console.log(y);
-    let x = document.getElementById("sq-1").offsetLeft;
-    console.log(x);
-    //document.getElementById("msg").innerHTML = "X: " + x + " Y : " + y;
-
-    if (x < 100) {
-      x = x + step;
-      document.getElementById("sq-1").style.left = x + "px"; //horizontal )
-      console.log(`After ${x}`);
+    function moveAFrame() {
+      elStyle = window.getComputedStyle(element);
       /*
-    if (y < 400) {
-      y = y + step;
-      document.getElementById("sq-1").style.top = y + "px"; // vertical movment
-    } else {
-      if (x < 800) {
-        x = x + step;
-        document.getElementById("sq-1").style.left = x + "px"; //horizontal move
-      }*/
+      console.log(elStyle.right);
+      console.log(elStyle.left);
+      console.log(elStyle.bottom);
+      console.log(elStyle.top);
+*/
+      value = elStyle.getPropertyValue(topOrLeft).replace("px", "");
+
+      let newLocation = Number(value) + frameDistance;
+      let beyondDestination =
+        (!isNegated && newLocation >= destination) ||
+        (isNegated && newLocation <= destination);
+      //Move square or clear setInterval
+      if (beyondDestination) {
+        element.style[topOrLeft] = destination + "px";
+        clearInterval(movingFrames);
+      } else {
+        element.style[topOrLeft] = newLocation + "px";
+      }
     }
+    //call the function to move the square
+    let movingFrames = setInterval(moveAFrame, 5);
+  }
+
+  function squareEngine2(element, direction, width, height, speed = 1000) {
+    /*Bit like a tower game - we are going to have a fixed route going 
+    Going from Top left to bottom left - alternate rows will be right to left
+    --X|
+    X--X */
+    // get the full .css value of the element
+    //Its overkill but want to get use to using it.
+    let squareStyle = window.getComputedStyle(element);
+    let squareLeft = squareStyle.left;
+    let squareRight = squareStyle.right;
+    let squareTop = squareStyle.top;
+    let squareBottom = squareStyle.bottom;
+
+    let distanceLeft = squareStyle
+      .getPropertyValue(topOrLeft)
+      .replace("px", "");
+    // going right
+    //need to compare objects position before and now - if its going towards up then going right
+    //when it matches screenWidth it goes down a few pixels and then goes left and distance goes towards 0.
   }
 }); //End of DOMContentLoaded
